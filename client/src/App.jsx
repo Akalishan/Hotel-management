c5e65cda55625b90dfd67594f78bb774586bb6da
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-08-15 23:03:55 +0530

    feat: Add AllRooms, RoomDetails, and MyBooking components with routing and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Footer } from "./components/Footer";
+import { AllRooms } from "./pages/AllRooms";
+import { RoomDetails } from "./pages/RoomDetails";
+import { MyBooking } from "./pages/MyBooking";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
@@ -12,6 +15,9 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my--bookings" element={<MyBooking />} />
         </Routes>
       </div>
       <Footer />
