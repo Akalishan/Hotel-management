ecba371bcaeec5b52e788623e4e63fa924c6c962
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-08-21 09:25:14 +0530

    feat: Integrate hotel registration functionality with form handling and toast notifications; add AppContext for global state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,16 @@ import { Layout } from "./pages/HotelOwner/Layout";
 import { Dashboard } from "./pages/HotelOwner/Dashboard";
 import { AddRoom } from "./pages/HotelOwner/AddRoom";
 import { ListRoom } from "./pages/HotelOwner/ListRoom";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
-
+  const { showHotelReg } = useAppContext();
   return (
     <div>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
-      {false && <HotelReg />}
+      {showHotelReg && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -25,9 +28,9 @@ const App = () => {
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBooking />} />
           <Route path="/owner" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="add-room" element={<AddRoom />} />
-            <Route path="list-room" element={<ListRoom />} />
+          <Route index element={<Dashboard />} />
+          <Route path="add-room" element={<AddRoom />} />
+          <Route path="list-room" element={<ListRoom />} />
           </Route>
         </Routes>
       </div>
