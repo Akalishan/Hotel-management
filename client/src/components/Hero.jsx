1027fd8bb9fb8cdb4db90d42ce16f6efe83c42a9
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-08-21 14:32:08 +0530

    feat: Implement room availability check and booking functionality in RoomDetails; add RecommendedHotels component for displaying filtered hotel recommendations

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 export const Hero = () => {
+  const [destination, setDestination] = useState("");
+  const { navigate, getToken, axios, setSearchedCities } = useAppContext();
+  const onSearch = async (e) => {
+    e.preventDefault();
+    navigate(`/rooms?destination=${destination}`);
+    //call api to save recent searched city
+    await axios.post(
+      "api/user/store-recent-search",
+      { recentSearchCity: destination },
+      { headers: { Authorization: `Bearer ${await getToken()}` } }
+    );
+
+    //add destination to searchedcities max 3 recent searched cities
+    setSearchedCities((prevSearchedCities) => {
+      const updatedSearchedCities = [...prevSearchedCities, destination];
+      if (updatedSearchedCities.length > 3) {
+        updatedSearchedCities.shift();
+      }
+      return updatedSearchedCities;
+    });
+  };
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
       <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">
@@ -15,13 +37,18 @@ export const Hero = () => {
         hotels and resorts. Start your journey today.
       </p>
 
-      <form className="bg-white text-gray-500 rounded-lg px-6 py-4  mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
+      <form
+        onSubmit={onSearch}
+        className="bg-white text-gray-500 rounded-lg px-6 py-4  mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto"
+      >
         <div>
           <div className="flex items-center gap-2">
             <img src={assets.calenderIcon} alt="" className="h-4" />
             <label htmlFor="destinationInput">Destination</label>
           </div>
           <input
+            onChange={(e) => setDestination(e.target.value)}
+            value={destination}
             list="destinations"
             id="destinationInput"
             type="text"
@@ -75,7 +102,7 @@ export const Hero = () => {
         </div>
 
         <button className="flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1">
-                      <img src={assets.searchIcon} alt="searchIcon" className="h-7"/>
+          <img src={assets.searchIcon} alt="searchIcon" className="h-7" />
 
           <span>Search</span>
         </button>
--- client/src/pages/MyBooking.jsx
+++ client/src/pages/MyBooking.jsx
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Tittle } from "../components/Tittle";
-import { assets, userBookingsDummyData } from "../assets/assets";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 export const MyBooking = () => {
-  const [bookings, setBookings] = useState(userBookingsDummyData);
+  const { axios, getToken, user } = useAppContext();
+  const [bookings, setBookings] = useState([]);
+  const fetchUserBookings = async () => {
+    try {
+      const { data } = await axios.get("/api/bookings/user", {
+        header: { Authorization: `Bearer ${await getToken()}` },
+      });
+      if (data.success) {
+        setBookings(data.bookings);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchUserBookings();
+    }
+  }, [user]);
+
   return (
     <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
       <Tittle
@@ -67,14 +91,14 @@ export const MyBooking = () => {
             <div className="flex flex-col items-start justify-center pt-3 ">
               <div className="flex items-center gap-2">
                 <div
-                  className={
-                    `h-3 w-3 rounded-full ${booking.isPaid ? "bg-green-500" : "bg-red-500" }`
-                  }
+                  className={`h-3 w-3 rounded-full ${
+                    booking.isPaid ? "bg-green-500" : "bg-red-500"
+                  }`}
                 ></div>
                 <p
-                  className={
-                    `text-sm ${booking.isPaid ? "text-green-500" : "text-red-500"}`
-                  }
+                  className={`text-sm ${
+                    booking.isPaid ? "text-green-500" : "text-red-500"
+                  }`}
                 >
                   {booking.isPaid ? "paid" : "Unpaid"}
                 </p>
