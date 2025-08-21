77d18ae0df7f3c2f371c3f934d78df46a6566d94
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-08-21 12:18:20 +0530

    feat: Integrate room fetching and filtering functionality in FeaturedDestination and AllRooms components

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -16,7 +16,21 @@ export const AppProvider = ({children}) => {
   const [isOwner, setIsOwner] = useState(false);
   const [showHotelReg, setShowHotelReg] = useState(false);
   const [searchedCities, setSearchedCities] = useState([]);
+  const [rooms,setRooms] = useState([]);
 
+const fetchRooms=async()=>{
+  try {
+    const{data}=await axios.get('/api/rooms')
+    if(data.success){
+      setRooms(data.rooms)
+    }else{
+      toast.error(data.message)
+    }
+  } catch (error) {
+    toast.error(error.message)
+  }
+}
+  
   const fetchUser = async () => {
     try {
       const {data} = await axios.get('/api/user', {
@@ -41,6 +55,10 @@ export const AppProvider = ({children}) => {
       fetchUser();
     }
   }, [user]);
+
+  useEffect(()=>{
+    fetchRooms();
+  },[])
   const value = {
     currency,
     navigate,
@@ -53,6 +71,8 @@ export const AppProvider = ({children}) => {
     setShowHotelReg,
     searchedCities,
     setSearchedCities,
+    rooms,
+    setRooms
   };
 
   return (<AppContext.Provider value={value}>{children}</AppContext.Provider>);
